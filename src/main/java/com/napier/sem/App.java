ca8daad023e1f617eeff7c15080adcd32c6e72b7
iainmclachlan <iainmcl1990@gmail.com>
2019-02-06 12:31:08 +0000

    Changed pom.xml

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -25,34 +25,48 @@ public class App {
     //Connection To MySQL Database
     private Connection con = null;
 
-    public void connect() {
-        try {
-            //Load Database Driver
+    /**
+     * Connect to the Database
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
             Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Could not Load SQL Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to Database...");
-
-            try {
-                //Wait for DB to Start
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
                 Thread.sleep(30000);
-                //Connect To database
+                // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-                System.out.println("Connection Successful");
+
+                System.out.println("Successfully connected");
                 break;
-            } catch (SQLException sqle) {
+            }
+            catch (SQLException sqle)
+            {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
-                System.out.println("Thread interrupted, This should not happen");
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
+
 
     public void disconnect() {
         if (con != null) {
@@ -67,47 +81,50 @@ public class App {
 
     public Employee getEmployee(int ID)
     {
-        try {
-            //Create a SQL Statement
+        try
+        {
+            // Create an SQL statement
             Statement stmt = con.createStatement();
-            //Create String for SQL Statement
+            // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name"
-                            + "FROM employees"
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
                             + "WHERE emp_no = " + ID;
-
-            //Execute SQL Statement
+            // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-
-            //Return new employee if valid
-            //Check if Returned
-            if (rset.next()) {
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
                 return emp;
-            } else
+            }
+            else
                 return null;
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get Employee Details");
+            System.out.println("Failed to get employee details");
             return null;
         }
     }
 
-    public  void displayEmployee(Employee emp)
+    public void displayEmployee(Employee emp)
     {
-        if(emp != null )
+        if (emp != null)
         {
             System.out.println(
                     emp.emp_no + " "
-                    + emp.first_name + " "
-                    + emp.last_name + "\n"
-                    + emp.title + "\n"
-                    + "Salary: " + emp.salary + "\n"
-                    + emp.dept_name + "\n"
-                    + "Manager: " + emp.manager + "\n");
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 }
